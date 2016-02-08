e87ed5f6703fbe825463204d0b8193d341e4233b
cupidon4uk <mylyanyk.ivan@gmail.com>
2016-02-09 00:09:41 +0200

    improved appearance of the description field

--- content.js
+++ content.js
@@ -6,20 +6,29 @@ div.id = 'myDivId';
 div.style.position = 'absolute';
 div.style.top = '10px';
 div.style.left = '10px';
-div.style.width = '15px';   
 div.style.height = '15px';
-div.style.backgroundColor = 'red';
+div.style.backgroundColor = '#000000';
+div.style.fontSize = '12px';
+div.style.paddingLeft = '4px';
+div.style.paddingRight = '4px';
+div.style.color = 'white';
 
 $("#myDivId").css('z-index', 8675309);
 
 function tagging(imgSrc) {
 	console.log('start tagging...');
 	var df = "http://www.clarifai.com/img/metro-north.jpg";
-	var url = "https://api.clarifai.com/v1/tag?access_token=HXgnsLDbsyG3xeJ1Ww14OhSjwMFiZ3&url=" + imgSrc;
+	var access_token = "w9VBZjIfxJovNdjHS44KNS5EgrXx2U";
+	var url = "https://api.clarifai.com/v1/tag?access_token=" + access_token + "&url=" + imgSrc;
 	$.get(url, function(data) {
+		var _class = data.results[0].result.tag.classes[0];
 		console.log(data.results[0].result.tag.classes);
+		console.log(_class);
+		$("#myDivId").html(_class);
 	});
 }
+
+console.log(Clarifai);
 
 $("img").on({
     mouseenter: function (e) {
